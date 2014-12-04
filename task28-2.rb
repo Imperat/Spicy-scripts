bed8c107555d8b049d56b7fe6bcdf0347edbe64d
Imperat <power_2007_pro@mail.ru>
2014-12-04 12:22:41 +0300

    Update task28-2.rb

--- task28-2.rb
+++ task28-2.rb
@@ -1,21 +1,23 @@
 class CofeeMachine
-  Size = 23
-  #Ингридиенты в наличии
-  component = {"coffee" => 0, "team" => 0, "chocolate" => 0, "cream" => 0, "sugar" => 0, "lemon" => 0}
-  #Рецепты напитков
-  Coffee = {"coffee" => 1, "sugar" => 4}
-  Tea = {"tea" => 1, "sugar" => 4, "lemon" => 1}
-  Chocolate = {"chocolate" => 1, "sugar" => 1}
-  Cream = {"cream" => 1, "sugar" => 0}
-  #Каждому напитку ставим в соответствие рецепт
-  Recipes = {"Coffee" => Coffee, "Tea" => Tea, "Chocolate" => Chocolate, "Cream" => Cream}
+
+  def initialize ()
+    @Size = 23
+    #Ингридиенты в наличии
+    @component = {"coffee" => 0, "team" => 0, "chocolate" => 0, "cream" => 0, "sugar" => 0, "lemon" => 0}
+    #Рецепты напитков
+    @Coffee = {"coffee" => 1, "sugar" => 4}
+    @Tea = {"tea" => 1, "sugar" => 4, "lemon" => 1}
+    @Chocolate = {"chocolate" => 1, "sugar" => 1}
+    @Cream = {"cream" => 1, "sugar" => 0}
+    #Каждому напитку ставим в соответствие рецепт
+    @Recipes = {"Coffee" => @Coffee, "Tea" => @Tea, "Chocolate" => @Chocolate, "Cream" => @Cream}
+  end
   
   def load ()
-  component.each_key { |key| component[key] = Size}
-  end
-  def hi ()
-  puts "lololololo"
-  end
+    @component.each_key { |key| @component[key] = @Size}
   end
   
-  puts "hello"
+  end
+  
+  cf = CofeeMachine.new
+  cf.load
