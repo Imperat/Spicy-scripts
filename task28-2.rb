1c9f440f372d28b99d1098ff217b9a53b54cac70
Imperat <power_2007_pro@mail.ru>
2014-12-09 16:40:14 +0300

    Update task28-2.rb

--- task28-2.rb
+++ task28-2.rb
@@ -1,21 +1,19 @@
 class CofeeMachine
 
   def initialize ()
-    
     #Ингридиенты в наличии
     @component = {"coffee" => 0, "tea" => 0, "chocolate" => 0, "cream" => 0, "sugar" => 0, "lemon" => 0}
-    #Рецепты напитков
-    @Coffee = {"coffee" => 1, "sugar" => 4}
-    @Tea = {"tea" => 1, "sugar" => 4, "lemon" => 1}
-    @Chocolate = {"chocolate" => 1, "sugar" => 1}
-    @Cream = {"cream" => 1, "sugar" => 0}
-    #Каждому напитку ставим в соответствие рецепт
-    @offers = {"Coffee" => 0, "Tea" => 0, "Chocolate" => 0, "Cream" => 0}
+    #Заказы
+    @offers = {}
+
+    @Size = 23
+    @Recipes = {"Coffee"    => {"coffee" => 1, "sugar" => 4}, 
+             "Tea"       => {"tea" => 1, "sugar" => 4, "lemon" => 1},
+             "Chocolate" => {"chocolate" => 1, "sugar" => 1},
+             "Cream"     => {"cream" => 1, "sugar" => 0}}
   end
 
-  Size = 23
-  Recipes = {"Coffee" => @Coffee, 
-             "Tea" => @Tea, "Chocolate" => @Chocolate, "Cream" => @Cream}
+
   def order (rec, changes)
     changes.each do |k, v|
       if k == "sugar" and v > 9 
@@ -25,57 +23,36 @@ class CofeeMachine
       end
     end
     #
-    if rec == "Coffee"
-      tmp = @Coffee
-      changes.each_key{
-        |key|
-        if key != "sugar" and key != "lemon" and key != "coffee"
-          changes.delete(key)
-        end
-      }
-    elsif rec == "Tea"
-      tmp = @Tea
-      changes.each_key{
-        |key|
-        if key != "sugar" and key != "lemon" and  key !=  "tea"
-          changes.delete(key)
-        end
-      }
-    elsif rec == "Chocolate"
-      tmp = @Chocolate
-      changes.each_key{
-      |key|
-      if key != "sugar" and key != "lemon" and key !=  "chocolate"
-        changes.delete(key)
-      end
-      }
-    elsif rec == "Cream"
-      tmp = @Cream
-      changes.each_key{
-       |key|
-       if key != "sugar" and key != "lemon" and key !=  "cream"
-         changes.delete(key)
-       end
-      }
+    if @Recipes.key?(rec)
+      tmp = @Recipes[rec]
     else
       return nil
     end
-    tmp.merge! (changes) {
-     |key, old, news|
+
+    changes.each_key{ |key|
+      if key != "sugar" and key != "lemon" and not @Recipes.key?(key)
+        changes.delete(key)
+      end
+      }
+
+    tmp.merge! (changes) do |key, old, news|
      if tmp.key?(key) != nil or key == "sugar" or key == "lemon"
        news = old + news
      end
-    }
-    tmp.each{
-      |k, v|
-      @component[k] -= v
-    }
+    end
+    tmp.each do |k, v|
+      if @component[k] - v > 0
+        @component[k] -= v
+      else
+        nil
+      end
+    end
     const = true
-    @component.each_value{
-      |v| if v < 0 then const = false end
-    }
+    @component.each_value do |v| 
+      if v < 0 then const = false end
+    end
     if const
-      @offers[rec] += 1
+      @offers[rec] ? @offers[rec]+=1 : @offers[rec]=1
     else
       return nil
     end
@@ -97,5 +74,11 @@ end
   
   cf = CofeeMachine.new
   cf.load
+  print cf.status, "\n"
   cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  print cf.status, "\n"
+  cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  print cf.status, "\n"
+  cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  print cf.status, "\n"
   print cf.stat
