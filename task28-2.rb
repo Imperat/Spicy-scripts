8584d9ac199adc5809492f0e0a875946405c188c
Imperat <power_2007_pro@mail.ru>
2014-12-10 01:34:33 +0300

    Update task28-2.rb

--- task28-2.rb
+++ task28-2.rb
@@ -1,28 +1,24 @@
 class CofeeMachine
 
   def initialize ()
-    #Ингридиенты в наличии
     @component = {"coffee" => 0, "tea" => 0, "chocolate" => 0, "cream" => 0, "sugar" => 0, "lemon" => 0}
-    #Заказы
     @offers = {}
-
     @Size = 23
     @Recipes = {"Coffee"    => {"coffee" => 1, "sugar" => 4}, 
-             "Tea"       => {"tea" => 1, "sugar" => 4, "lemon" => 1},
-             "Chocolate" => {"chocolate" => 1, "sugar" => 1},
-             "Cream"     => {"cream" => 1, "sugar" => 0}}
+                "Tea"       => {"tea" => 1, "sugar" => 4, "lemon" => 1},
+                "Chocolate" => {"chocolate" => 1, "sugar" => 1},
+                "Cream"     => {"cream" => 1, "sugar" => 0}}
   end
 
 
-  def order (rec, changes)
+  def order rec, changes
     changes.each do |k, v|
       if k == "sugar" and v > 9 
-        v = 9
+        changes["sugar"] = 9
       elsif v.abs > 1 and k != "sugar"
         changes[k] = v/v.abs
       end
     end
-    #
     if @Recipes.key?(rec)
       tmp = @Recipes[rec]
     else
@@ -34,28 +30,24 @@ class CofeeMachine
         tmp[key] += changes[key]
       end
     end
-    unless tmp["lemon"]
+    unless tmp["lemon"] or changes["lemon"] <= 0
       tmp["lemon"] = changes["lemon"]
     end
-    unless tmp["sugar"]
+    unless tmp["sugar"] or changes["lemon"] <= 0
       tmp["sugar"] = changes["sugar"]
     end
+    temp = @component.clone
     tmp.each do |k, v|
-      '''if @component[k] - v > 0
-        @component[k] -= v
+      if temp[k] - v > 0
+        temp[k] -= v
       else
+        const = false
         nil
       end
-      '''
-      temp = @component
-      
-      unless temp[k]-v > 0 ? temp[k] -= v : nil
-        const = false
-        break
-      end
-      @component = temp
+     # if f then @component = temp end
     end
     if const
+      @component = temp.clone
       @offers[rec] ? @offers[rec]+=1 : @offers[rec]=1
     else
       return nil
@@ -78,15 +70,15 @@ end
   
   cf = CofeeMachine.new
   cf.load
-  print cf.status, "\n"
+  puts cf.status
   cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
-  print cf.status, "\n"
+  puts cf.status
   cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
-  print cf.status, "\n"
+  puts cf.status
   cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
-  print cf.status, "\n"
+  puts cf.status
   cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
-  print cf.status, "\n"
+  puts cf.status
   cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
-  print cf.status, "\n"
+  puts cf.status
   print cf.stat
