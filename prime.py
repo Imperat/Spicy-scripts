6efdb3c2943671366d4d7fc2dda6c96732cd721b
Imperat <power_2007_pro@mail.ru>
2014-12-24 02:43:12 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,16 +4,17 @@ def Prime (N, matrix, Tree = [0], res = []):
     index = -1
     for j in Tree:
         for i in range(len(matrix[j])):
-            if matrix[j][i] < minim and i not in Tree:
-                minim = i
+            if 0 < matrix[j][i] < minim and i not in Tree:
+                minim = matrix[j][i]
                 index, indexj = i, j
     if index != -1:
-        res.append((index, indexj))
+        res.append((index, indexj, matrix[index][indexj]))
         return Prime(N, matrix, Tree + [index], res)
     else:
         return res
 
 tmp = Prime(3, [[0, 5, 1], [5,0,3], [1,3,0]])
 #tmp = Prime(4, [[0, 1, 1, 1],[1, 0, 1, 1],[1, 1, 0, 1],[1, 1, 1, 0]])
+print "a - b - len"
 for i in tmp:
     print i
