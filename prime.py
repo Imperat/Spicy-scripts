a179d8085baacca7a181b0cb537d47b9174f6c35
Imperat <power_2007_pro@mail.ru>
2014-12-24 02:15:09 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,29 +1,19 @@
-def Prime (N, matrix):
-    INF = 10000000
-    used = [False]*N
-    min_e = [INF]*N
-    sel_e = [-1]*N
-    min_e[0] = 0
-    sel_e[0] = 1
-    res = []
-    for i in range(N):
-        v = -1
-        for j in range(N):
-            if (not used[i]) and (v == -1 or (min_e[j] < min_e[v] and v!= -1)):
-                v = j
-        if min_e[v] == INF:
-            print "Error!"
-            return False
+# -*- coding: utf-8 -*-
+def Prime (N, matrix, Tree = [0], res = []):
+    minim = 100000
+    index = -1
+    for j in Tree:
+        for i in matrix[j]:
+            if i < minim and matrix[j].index(i) not in Tree:
+                minim = i
+                index, indexj = matrix[j].index(i), j
+    if index != -1:
+        res.append((index, indexj))
+        print "в оставное дерево добавлена вершина " + str(index)
+        return Prime(N, matrix, Tree + [index], res)
+    else:
+        return Tree
 
-        used[v] = True
-        if sel_e[v] != -1:
-            res.append((v, sel_e[v]))
-
-        for to in range(N):
-            if matrix[v][to] < min_e[to]:
-                min_e[to] = matrix[v][to]
-                sel_e[to] = v
-    return res
-
-tmp = Prime(3, [[0, 5, 1], [5,0,3], [1,3,0]])
+#tmp = Prime(3, [[0, 5, 1], [5,0,3], [1,3,0]])
+tmp = Prime(4, [[0, 1, 1, 1],[1, 0, 1, 1],[1, 1, 0, 1],[1, 1, 1, 0]])
 print tmp
