0418f244e884f6c237ed19ee2fa18f9bd904661d
Imperat <power_2007_pro@mail.ru>
2015-01-24 18:45:45 +0300

    Update task28-02.rb

--- task28-02.rb
+++ task28-02.rb
@@ -1,84 +1,90 @@
-class MyConditioner < Conditioner
-  def change_tact new_tact
-  	@tact = new_tact
+class CofeeMachine
+
+  def initialize
+    @component = {"coffee" => 0, "tea" => 0, "chocolate" => 0, "cream" => 0, "sugar" => 0, "lemon" => 0}
+    @offers = {}
   end
-end
+  Size = 23
+  Recipes = {"Coffee"    => {"coffee" => 1, "sugar" => 4}, 
+             "Tea"       => {"tea" => 1, "sugar" => 4, "lemon" => 1},
+             "Chocolate" => {"chocolate" => 1, "sugar" => 1},
+             "Cream"     => {"cream" => 1, "sugar" => 0}}
 
-
-class MyHeater < Heater 
-
-end
-
-
-class MyClimateControl < ClimateControl
-  def check_conditioner
-    if @home.temperature > @cond_start # если превышен темп. барьер
-      @home.conditioner(0).set_on         # перевести в режим работы первый
-      if @home.temperature - @cond_start >= 4
-      	@home.conditioner(1).set_on
+  def order rec, changes
+    changes.each do |k, v|
+      if k == "sugar" and v > 9 
+        changes["sugar"] = 9
+      elsif v.abs > 1 and k != "sugar"
+        changes[k] = v/v.abs
       end
+    end
+    if Recipes.key?(rec)
+      tmp = Recipes[rec].clone
     else
-      @home.conditioner(0).set_off        # перевести в режим ожидания
-      @home.conditioner(1).set_off        # перевести в режим ожидания
+      return nil
+    end
+    const = true
+    tmp.each_key do |key|
+      if changes.key?(key)
+        tmp[key] += changes[key]
+        if tmp[key] < 0 then tmp[key] = 0 end
+      end
+    end
+    ["lemon", "sugar"].each do |x|
+      unless tmp.key?(x)
+        if changes[x]
+          if changes[x] > 0 then tmp[x] = changes[x] end
+        end
+      end
+    end
+    temp = @component.clone
+    tmp.each do |k, v|
+      if temp[k] - v > 0
+        temp[k] -= v
+      else
+        const = false
+        nil
+      end
+    end
+    if const
+      @component = temp.clone
+      @offers[rec] ? @offers[rec]+=1 : @offers[rec]=1
+      true
+    else
+      nil
     end
   end
+
+  def load ()
+    @component.each_key { |key| @component[key] = Size }
+    self
+  end
+  
+  def status ()
+    @component
+  end
+
+  def stat ()
+    @offers
+  end
+
 end
-
-
-class MyReporter < Reporter
-  def cond_status_s
-    "conditioner one: " + bool_to_on_off_s(@home.conditioner(0).status) + " " +
-    "conditioner two: " + bool_to_on_off_s(@home.conditioner(1).status)
-  end
-end
-
-
-class MyWeather < Weather
-
-end
-
-class MyCyclon < Cyclon
-    Cyclons = 
-    [[-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2],
-     [-1.5, -1, -0.5, 0, 0, 0],
-     [0, 0, 0, 0.5, 1, 1.5],
-     [-1, -0.5, 0, 0, 0, 0.5, 1],
-     [-0.5, 0, 0.5],
-     [-2.1, -1.8, -1.5, -1.2, -0.9, -0.6, -0.3, 0, 0.3, 0.6, 0.9, 1.2, 1.5, 1.8, 2.1]
-    ] 
-
-    def self.new_cyclon weather
-    # берется случайный представитель списка Cyclons 
-    # и с ним создается новый циклон
-    MyCyclon.new(MyCyclons.sample, weather)
-  end
-end
-
-class MyGlasshouse < Glasshouse
-  def set_glasshouse
-    @conditioner0 = MyConditioner.new self
-    @conditioner1 = MyConditioner.new self
-    @conditioner0.change_tact 0.4
-    @conditioner1.change_tact 0.3
-    @heater = Heater.new self
-    @climate_control = MyClimateControl.new(self, 15, 25)
-    @reporter = MyReporter.new self
-  end
-
-  def list_of_processes
-    all_system = [@weather, @conditioner0, @conditioner1, @heater, @climate_control, @reporter]
-    all_system.map { |syst| syst.get_runner }
-  end
-
-  def conditioner n
-    n == 0 ? @conditioner0 : @conditioner1
-  end
-
-  def temperature
-    # складывается как изменение температуры, вследствие погоды
-    # минус изменение, сгенерированное кондиционером
-    # плюс изменение, сгенерированное обогревателем
-    @weather.temperature - @conditioner0.change - @conditioner1.change + @heater.change
-  end
-end
-
+  
+  cf = CofeeMachine.new
+  cf.load
+  puts cf.status
+  cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  puts cf.status
+  cf.order("Coffee", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  puts cf.status
+  cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  puts cf.status
+  puts cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  puts cf.status
+  cf.order("Tea", {"lemon" => 4, "tea" => 3, "sugar" => 2, "coffee" =>2})
+  cf.order("Cream", {"sugar" => 2})
+  cf.order("Cream", {"sugar" => -1})
+  cf.order("Cream", {"sugar" => 1})
+  cf.order("Cream", {"sugar" => 1})
+  puts cf.status
+  print cf.stat
