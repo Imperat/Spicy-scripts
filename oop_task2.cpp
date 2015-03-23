6e4d0c45d0c9e21f0facc08847f256538e6e440c
Imperat <power_2007_pro@mail.ru>
2015-03-23 09:30:50 +0300

    Update oop_task2.cpp

--- oop_task2.cpp
+++ oop_task2.cpp
@@ -2,6 +2,10 @@
 #include <vector>
 #include <cmath>
 
+
+#define D 0.05
+#define PT Point
+
 using namespace std;
 
 class Point {
@@ -9,7 +13,7 @@ public:
 	double x,y;
 	double altituge;
 
-	Point (int nx, int ny, int altg) {
+	Point (double nx, double ny, double altg) {
 		x = nx;
 		y = ny;
 		altituge = altg;
@@ -31,39 +35,60 @@ private:
 		return way[i];
 	}
 
-public:
-	Point getPlace(){
-		a++;
-		if a>
-		return way[a];
-	}
-
-	void kill (double nx, double ny, double naltituge){
-		if (
-				(abs(nx - x) < d)
-			&&  (abs(ny - y) < d)
-			&&  (abs(naltituge - altituge < d))
-			) avalibale = false;
-
-	}
-	/*double getAltituge(int i){
+	double getAltituge(int i){
 		return way[i].altituge;
 	}
-
 	double getSpeed (int i){
 		if (i==0) return 0;
 		else
 			return getPoint(i).getLength() - getPoint(i-1).getLength();
 	}
-
 	double getSpeedUp (int i){
 			if (i<=1) return 0;
 			else
 				return getSpeed(i) - getSpeed(i-1);
-	}*/
+	}
+
+public:
+
+	Plan (vector <Point> w){
+		way = w;
+		avalibale = true;
+		a = 0;
+
+	}
+
+	Point getPlace(){
+		a++;
+		if (a < way.size())
+			return way[a];
+		else return Point(0, 0, 0);
+	}
+
+	void kill (double nx, double ny, double naltituge){
+		double x = getPoint(a).x;
+		double y = getPoint(a).y;
+		double altituge = getPoint(a).altituge;
+
+		if (
+				(abs(nx - x) < D)
+			&&  (abs(ny - y) < D)
+			&&  (abs(naltituge - altituge < D))
+			) avalibale = false;
+
+	}
+	bool isAvalibale (){
+		return avalibale;
+	}
 };
 
 int main(){
+	vector <Point> p;
+
+	p.push_back(PT(0,100,4));
+	
+
+	Plan a = Plan (p);
 
 	return 0;
 }
